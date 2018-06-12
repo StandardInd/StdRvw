52edef58bf0d1b5d9326ce4f13f3c8e6d580e7e9
adriano <huadriano2001@gmai.com>
2018-06-12 18:36:10 +0200

    ottimizzazione di activity e app_bar

--- app/src/main/java/stdind/standardreview/activity_categorie.java
+++ app/src/main/java/stdind/standardreview/activity_categorie.java
@@ -25,22 +25,24 @@ public class activity_categorie extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categorie);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        findViewById(R.id.include_main).setVisibility(View.INVISIBLE);
+        findViewById(R.id.include_cat).setVisibility(View.VISIBLE);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_cat);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_cat);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_cat);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -79,6 +81,7 @@ public class activity_categorie extends AppCompatActivity
 
         if (id == R.id.nav_home) {
             startActivity(new Intent(activity_categorie.this, MainActivity.class));
+
         } else if (id == R.id.nav_account) {
 
         } else if (id == R.id.nav_myreview) {
@@ -93,7 +96,7 @@ public class activity_categorie extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_cat);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
